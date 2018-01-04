f2f00e0f3dc51ac3e180af6a7abd9d140d5f6295
LakshmananVignesh <smartlakshmanan16@gmail.com>
2018-01-04 13:08:34 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,42 @@
-var express = require('express');
-var app = express();
+var http = require("http");
+ const https = require('https');
+http.createServer(function (request,response){
+ response.writeHead(200,{'Content-Type':'text/plain'});
+ response.end("Hello World\n");
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+var post_data={
+	'departmentId':'127254000000006907',
+	'status':'open',
+	'subject':'ticket created via node js',
+	'contactId':'127254000000069619',
+	'assigneeId':'127254000000064003'
+}
+var postData = JSON.stringify(post_data);
+console.log('postData hallo'+postData);
+const options={
+	hostname:'desk.zoho.com',
+	port:443,
+	path:'/api/v1/tickets',
+	method:'POST',
+	headers:{
+		'Authorization':'Zoho-authtoken a86eb1beed861cbe670b34c7d27e4bf6',
+		'orgId':'525025162'
+	}
+};
+console.log('options hallo'+options);
+const requ = https.request(options,(res)=>{
+	console.log('hallo');
+	res.setEncoding('utf8');
+	res.on('data',(d)=>{
+		process.stdout.write(d);
+	});
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+requ.write(postData);
+
+requ.on('error',(e)=>{
+	console.error(e);
 });
+requ.end();
+}).listen(8080);
+console.log('Server running at http://127.0.0.1:8081/');
